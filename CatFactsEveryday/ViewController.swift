1d3ea53bbeff5a455027e6cca7f47e96b60d9fb9
Anton <antony.adamson@gmail.com>
2023-09-23 11:19:36 +0600

    Initial Commit

---
+++ CatFactsEveryday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatFactsEveryday
+//
+//  Created by Антон Адамсон on 23.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
